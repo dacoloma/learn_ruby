a422ace976e678e5ac1292575660f86ef154c073
D.Coloma <dnl.coloma@gmail.coma>
2018-01-16 22:30:01 +0100

    02 done

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -10,3 +10,15 @@ end
 def sum(lst)
     return lst.reduce(:+) if lst.length > 0 else 0
 end
+
+def multiply(lst)
+    return lst.reduce(1,  :*) if lst.length > 0 else 0
+end
+
+def power(x,y)
+    return x ** y
+end
+
+def factorial(n)
+    return (1..n).reduce(1, :*)
+end
