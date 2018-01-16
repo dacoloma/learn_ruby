62c7e529bb9e8194f38c410d4f09e2bdc5953418
D.Coloma <dnl.coloma@gmail.coma>
2018-01-16 19:06:15 +0100

    final

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -3,14 +3,10 @@ def translate(str)
     consonne="bcdfghjklmnpqrstvwxz"
     pig = "ay"
     tab,tab2, tab_word=[],[],[]
-
     tab=str.split
-
     tab2=tab.map do |value|
-
         i = 0
         tab_word = value.split("")
-
         while consonne.include? value[i]
             if value[i+1] == 'u'
                 tab_word.rotate!(1)
@@ -18,10 +14,7 @@ def translate(str)
             tab_word.rotate!(1)
             i += 1
         end
-
         value=tab_word.join("") + pig
-
     end
-
     return str=tab2.join(" ")
 end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,10 @@
 class Book
 # write your code here
+    #attr_accessor :title
+
+    def title(value)
+        title= value #.split.each{|s| s.capitalize!}.join(" ")
+
+    end
+
 end
