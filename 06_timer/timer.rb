926f343794b2ff68eb8465abb776369ee3b5781f
D.Coloma <dnl.coloma@gmail.coma>
2018-01-21 15:45:38 +0100

    06 done

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,18 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+  def seconds
+      @seconds = 0
+  end
+
+  def time_string
+    h = @seconds / 3600
+    m = (@seconds % 3600 ) / 60
+    s = (@seconds % 3600 ) % 60
+    h=format("%02d",h)
+    m=format("%02d",m)
+    s=format("%02d",s)
+
+    return "#{h}:#{m}:#{s}"
+  end
 end
